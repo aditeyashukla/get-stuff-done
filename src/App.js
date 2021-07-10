3b6fee33a40ffa868e47216688d39ab6e0394a80
aditeyashukla <aditeyashukla@gmail.com>
2021-07-10 23:45:12 +0530

    changed menu to be fabs

--- src/App.js
+++ src/App.js
@@ -17,11 +17,18 @@ import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import Paper from '@material-ui/core/Paper';
+// import Fab from '@material-ui/core/Fab';
 
 //Material Icons
 import HomeIcon from '@material-ui/icons/Home';
 import DateRangeIcon from '@material-ui/icons/DateRange';
 import SettingsIcon from '@material-ui/icons/Settings';
+import AddIcon from '@material-ui/icons/Add';
+import MenuIcon from '@material-ui/icons/Menu';
+
+//Fab menu
+import { Fab, Action } from 'react-tiny-fab';
+import 'react-tiny-fab/dist/styles.css';
 
 //Local Components
 import LoginPage from './components/login/loginPage';
@@ -30,33 +37,34 @@ import HomePage from './components/homepage/homePage';
 const useStyles = makeStyles({
   bottomNav: {
     width: '100%',
-    backgroundColor:'#485051',
+    backgroundColor: '#485051',
     position: 'absolute',
     bottom: 0
-    
+
   },
-  bottomNavIcon:{
+  bottomNavIcon: {
   },
-  mainPaper:{
+  mainPaper: {
     width: '90%',
-    height: '88%',
+    height: '98%',
     margin: '0% 5% 3% 5%',
-    backgroundColor:'#2d2f2f'
+    backgroundColor: '#2d2f2f'
   }
 });
 
 function App() {
   const classes = useStyles();
   const [value, setValue] = React.useState('home');
+  const [fabH, setfabH] = React.useState(0)
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   const PageReturn = () => {
-    switch(value){
-      case 'home': 
-        return <HomePage/>;
+    switch (value) {
+      case 'home':
+        return <HomePage />;
       case 'weekview':
         return "week";
       case 'settings':
@@ -65,33 +73,101 @@ function App() {
         return 'home'
     }
   }
+  // const theme = useTheme();
+  // const transitionDuration = {
+  //   enter: theme.transitions.duration.enteringScreen,
+  //   exit: theme.transitions.duration.leavingScreen,
+  // };
 
+  // const fabs = [
+  //   {
+  //     color: 'primary',
+  //     className: classes.fab,
+  //     icon: <AddIcon />,
+  //     label: 'Add',
+  //     textLabel:"",
+  //     variant: '',
+  //   },
+  //   {
+  //     color: 'primary',
+  //     className: classes.fab,
+  //     icon: <AddIcon />,
+  //     label: 'Add',
+  //     textLabel:"Add New Task",
+  //     variant: 'extended'
+  //   }
+
+  // ];
+
+  function fabHover() {
+    setfabH(1)
+  }
+  function fabOut() {
+    setfabH(0)
+  }
   return (
     <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
-    <div className="App">
-      <Paper className={classes.mainPaper} elevation={3}>
-      
-        {/* <button
+      <div className="App">
+        <Paper className={classes.mainPaper} elevation={3}>
+
+          {/* <button
           onClick={() => {
             firebase.auth().signOut();
           }}
         >
           Sign Out
         </button> */}
-      <FirebaseAuthConsumer>
-      {({ isSignedIn, user, providerId }) => {
-        console.log(user)
-        return isSignedIn ? (<PageReturn/>) : (<LoginPage/>)
-      }}
-    </FirebaseAuthConsumer>
+          <FirebaseAuthConsumer>
+            {({ isSignedIn, user, providerId }) => {
+              console.log(user)
+              return isSignedIn ? (<PageReturn />) : (<LoginPage />)
+            }}
+          </FirebaseAuthConsumer>
+          
+
+          <Fab
+            // mainButtonStyles={mainButtonStyles}
+            // actionButtonStyles={actionButtonStyles}
+            // style={style}
+            icon={"☰"}
+            // event={event}
+            alwaysShowTitle={true}
+          // onClick={}
+          >
+
+            <Action
+              text="Add New Task"
+            // onClick={handleEmailOnClick}
+            >
+              <AddIcon />
+            </Action>
+
+            <Action
+              text="Home"
+              onClick={(e) => setValue("home")}
+            >
+              <HomeIcon />
+            </Action>
+
+            <Action
+              text="Your Week"
+              onClick={(e) => setValue("weekview")}
+            // onClick={handleEmailOnClick}
+            >
+              <DateRangeIcon />
+            </Action>
+
+            <Action
+              text="Settings"
+              onClick={(e) => setValue("settings")}
+            >
+              <SettingsIcon />
+            </Action>
+
+          </Fab>
         </Paper>
-      <BottomNavigation value={value} showLabel={false} onChange={handleChange} className={classes.bottomNav}>
-      <BottomNavigationAction label="Home" value="home"  className={classes.bottomNavIcon} icon={<HomeIcon />} />
-      <BottomNavigationAction label="Your Week" className={classes.bottomNavIcon} value="weekview" icon={<DateRangeIcon />} />
-      <BottomNavigationAction label="Settings" className={classes.bottomNavIcon} value="settings" icon={<SettingsIcon />} />
-    </BottomNavigation>
-    
-    </div>
+
+      </div>
     </FirebaseAuthProvider>
   );
 }
--- src/components/task/taskComponent.js
+++ src/components/task/taskComponent.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -9,12 +9,19 @@ import RadioButtonUncheckedOutlinedIcon from '@material-ui/icons/RadioButtonUnch
 
 import './taskComponent.css'
 
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
+function handleClick(e, data) {
+    console.log(data.foo);
+}
+
 //Props:
 //Task Object
 //Check Function
 export default function TaskComponent() {
-    let rec = true;
+    let [complete, setComplete] = useState(false); //task complete
+    let rec = true; //reccuring
     let col = rec ? '#a9e3f3' : '#f3c1a9'
+
     const ColouredCheckbox = withStyles({
         root: {
             color: col,
@@ -25,29 +32,50 @@ export default function TaskComponent() {
         checked: {},
     })((props) => <Checkbox color="default" {...props} />);
 
-    return (
-        <Paper elevation={3} className={rec ? "task-paper reccuring" : "task-paper one-time"}>
-            <Grid
-                className={"task-grid"}
-                container
-                direction="row"
-                justifyContent="flex-start"
-                alignItems="flex-start"
-            >
-                <Grid item className={"task-checkbox"} >
-                    <FormControlLabel
-                        control={<ColouredCheckbox icon={<RadioButtonUncheckedOutlinedIcon fontSize="medium" />} checkedIcon={<CheckCircleIcon fontSize="medium" />} name="checkedH" />}
-                    />
-                </Grid>
-                <Grid item sm={6} className={"task-name"} >
-                    AA 102</Grid>
-                <Grid item sm={5} className={"task-days"} >M/W/F 9:30AM</Grid>
-                
-                
-            </Grid>
-            
+    function checkTask(e) {
+        let element = e.target
+        setComplete(element.checked)
+        element.classList.toggle("strikethrough");
+    }
 
-        </Paper>
+    return (
+        <>
+            <ContextMenuTrigger id="same_unique_identifier">
+                <Paper elevation={3} className={rec ? "task-paper reccuring" : "task-paper one-time"}>
+                    <Grid
+                        className={"task-grid"}
+                        container
+                        direction="row"
+                        justifyContent="flex-start"
+                        alignItems="flex-start"
+                    >
+                        <Grid item className={"task-checkbox"} >
+                            <FormControlLabel
+                                control={<ColouredCheckbox onChange={checkTask} checked={complete} icon={<RadioButtonUncheckedOutlinedIcon fontSize="medium" />} checkedIcon={<CheckCircleIcon fontSize="medium" />} name="checkedH" />}
+                            />
+                        </Grid>
+                        <Grid item sm={6} className={"task-name"} >
+                            <p className={complete ? "strikethrough" : undefined} style={{ margin: 0 }}>AA 102</p>
+                        </Grid>
+                        <Grid item sm={5} className={"task-days"} ><p className={complete ? "strikethrough" : undefined} style={{ margin: 0 }}>M/W/F 9:30AM</p></Grid>
+
+
+                    </Grid>
+                </Paper>
+            </ContextMenuTrigger>
+            <ContextMenu id="same_unique_identifier">
+                <MenuItem data={{ foo: 'bar' }} onClick={handleClick}>
+                    ContextMenu Item 1
+                </MenuItem>
+                <MenuItem data={{ foo: 'bar' }} onClick={handleClick}>
+                    ContextMenu Item 2
+                </MenuItem>
+                <MenuItem divider />
+                <MenuItem data={{ foo: 'bar' }} onClick={handleClick}>
+                    ContextMenu Item 3
+                </MenuItem>
+            </ContextMenu>
+        </>
     )
 
 }
